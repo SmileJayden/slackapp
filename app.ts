ac91eeedfb4934751a10e7006389e3c81322184d
SmileJayden <wkdwodud07@gmail.com>
2020-06-30 17:11:37 +0900

    add dotenv

--- app.ts
+++ app.ts
@@ -1,3 +1,29 @@
-import webClient from '@slack/web-api';
+import { WebClient } from '@slack/web-api';
+import * as dotenv from 'dotenv';
 
-console.log('hello world @slack/web-api from ts');
+dotenv.config();
+
+console.log('process.env.SLACK_TOKEN', process.env.SLACK_TOKEN);
+
+const port = process.env.PORT;
+console.log(`Your port is ${port}`);
+
+// Create a new instance of the WebClient class with the token read from your environment variable
+const web = new WebClient(process.env.SLACK_TOKEN);
+
+// The current date
+const currentTime = new Date().toTimeString();
+
+(async () => {
+  try {
+    // Use the `chat.postMessage` method to send a message from this app
+    await web.chat.postMessage({
+      channel: '#alarmrecruitmentapitest',
+      text: `The current time is ${currentTime}`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log('Message posted!');
+})();
